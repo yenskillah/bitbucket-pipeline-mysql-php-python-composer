1547ccaa64f144b2482f855ed171cf5d193b6776
yenskillah <sonnysidramos@gmail.com>
2016-11-29 14:09:02 +0800

    Create test.sh

---
+++ scripts/test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# exit on error
+set -e
+service mysqld start
+/usr/bin/mysqladmin -u $MYSQL_USERNAME password $MYSQL_PASSWORD
+mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
+mkdir -p /var/alsocan/config
+mkdir -p /var/alsocan/system
