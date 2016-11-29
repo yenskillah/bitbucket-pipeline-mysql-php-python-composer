49cd9bb2fd17f4b5cf9fc4f48b612541d828c196
Sonny Sid Ramos <sonny@matchmove.com>
2016-11-29 15:30:58 +0800

    updated docker and codeship steps

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,13 @@
 
 # exit on error
 set -e
+
+export MYSQL_HOST=localhost
+export MYSQL_USERNAME=root
+export MYSQL_PASSWORD=root
+
 service mysqld start
-# /usr/bin/mysqladmin -u $MYSQL_USERNAME password $MYSQL_PASSWORD
-# mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
+ /usr/bin/mysqladmin -u root password root
+ mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
 mkdir -p /var/alsocan/config
 mkdir -p /var/alsocan/system
