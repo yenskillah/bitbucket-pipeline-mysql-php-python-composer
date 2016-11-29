3a10057fc36cdae62ce90407f2b9eeb3fc4f06a8
Sonny Sid Ramos <sonny@matchmove.com>
2016-11-29 15:50:41 +0800

    updated docker and steps

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,7 +8,11 @@ export MYSQL_USERNAME=root
 export MYSQL_PASSWORD=root
 
 service mysqld start
- /usr/bin/mysqladmin -u root password root
- mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
+
+/usr/bin/mysqladmin -u root password root
+
+mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
 mkdir -p /var/alsocan/config
 mkdir -p /var/alsocan/system
+
+echo 'done'
