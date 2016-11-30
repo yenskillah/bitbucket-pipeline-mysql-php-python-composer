0e7902d602cacff6c0732e7cf423c72a9aab519c
Sonny Sid Ramos <sonny@matchmove.com>
2016-11-30 16:02:54 +0800

    update scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -16,14 +16,15 @@ fi
 mkdir -p /var/alsocan
 aws s3 sync s3://$AWS_S3_VPC/$AWS_S3_BUCKET/alsocan /var/alsocan/ --region=$AWS_DEFAULT_REGION --exact-timestamps
 set > /var/alsocan/system/bash
-cat $IH_DB_SQL | base64 -d > /var/alsocan/config/ih_db.sql
+$IH_DB_SQL > /var/alsocan/config/ih_db.sql
+
 echo 'Config Setup Done.'
 
 #mysql start and settings
 service mysqld start
 /usr/bin/mysqladmin -u root password root
 mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
-mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD havells_ih_uat < /var/alsocan/config/ih_db.sql
+mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD havells_ih_uat < /var/alsocan/config/ih_db.sql | base64 -d
 echo 'MySQL Done.'
 
 
