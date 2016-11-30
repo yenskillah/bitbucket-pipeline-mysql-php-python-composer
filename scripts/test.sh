fb580258fdffbd6cfcae2e03eddd16aae0e47f7e
Sonny Sid Ramos <sonny@matchmove.com>
2016-11-30 10:56:46 +0800

    update scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,6 @@ export MYSQL_HOST=localhost
 export MYSQL_USERNAME=root
 export MYSQL_PASSWORD=root
 
-
 # must be run inside the project. this is a hack for codeship
 if [ ! -z ${1+x} ]; then
   cd $1
@@ -20,10 +19,10 @@ service mysqld start
 mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
 mkdir -p /var/alsocan
 
-echo $AWS_EC2_VPC;
-echo $AWS_DEFAULT_REGION;
-echo $HOSTNAME;
+# echo $AWS_EC2_VPC;
+# echo $AWS_DEFAULT_REGION;
+# echo $HOSTNAME;
 
-aws s3 sync s3://vcard-vpc-be359cdb/asg-havells-ad-api-uat/alsocan /var/alsocan/ --region=ap-southeast-1 --exact-timestamps
+aws s3 sync s3://$AWS_S3_VPC/$AWS_S3_BUCKET/alsocan /var/alsocan/ --region=$AWS_DEFAULT_REGION --exact-timestamps
 
 echo 'done'
