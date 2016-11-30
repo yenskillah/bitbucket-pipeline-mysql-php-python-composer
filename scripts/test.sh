d07c0572983f85803493cd3536478bbc5d997cb6
Sonny Sid Ramos <sonny@matchmove.com>
2016-11-30 15:47:10 +0800

    update scripts/test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,20 +12,22 @@ if [ ! -z ${1+x} ]; then
   cd $1
 fi
 
-service mysqld start
-
-/usr/bin/mysqladmin -u root password root
-
-mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
+#config setup
 mkdir -p /var/alsocan
-
 aws s3 sync s3://$AWS_S3_VPC/$AWS_S3_BUCKET/alsocan /var/alsocan/ --region=$AWS_DEFAULT_REGION --exact-timestamps
-
 set > /var/alsocan/system/bash
+cat $IH_DB_SQL | base64 -d > /var/alsocan/config/ih_db.sql
+echo 'Config Setup Done.'
 
-cat /var/alsocan/system/bash
+#mysql start and settings
+service mysqld start
+/usr/bin/mysqladmin -u root password root
+mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD -e "CREATE DATABASE havells_ad_uat; CREATE DATABASE havells_ih_uat; CREATE DATABASE havells_vc_uat;"
+mysql -h$MYSQL_HOST -u$MYSQL_USERNAME -p$MYSQL_PASSWORD havells_ih_uat < /var/alsocan/config/ih_db.sql
+echo 'MySQL Done.'
+
 
 #install modules
 composer install
 
-echo 'done'
+echo 'Composer Install Done.'
